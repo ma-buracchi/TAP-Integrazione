77c572aaeef1fd7be1edc607da40d9c15c3a76fa
marco buracchi <ma.buracchi@gmail.com>
2018-05-19 15:38:45 +0200

    Component Scan annotation

--- TAP-web-integration/src/main/java/it/buracchi/ciphers/web/TapWebIntegrationApplication.java
+++ TAP-web-integration/src/main/java/it/buracchi/ciphers/web/TapWebIntegrationApplication.java
@@ -2,8 +2,10 @@ package it.buracchi.ciphers.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
 public class TapWebIntegrationApplication {
 
 	public static void main(String[] args) {
